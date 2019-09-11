7c4fd3763100cada23e24eff2e68a333c693249a
Levi Brown <rlevibrown@gmail.com>
2019-09-11 15:31:19 -0600

    added state and images

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const PlayerCard = ()=> {
+  const style = {
+    background: color
+  }
+  return(
+    <div style = {style} className="player-card">
+        {symbol}
     </div>
-  );
+  )
+}
+
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.symbols = ["rock", "paper", scissors, ]
+    this.state = { }
+  }
+
+  runGame = () => {
+    this.setState({
+      playerRed: this.symbols[Math.floor(Math.random()*3)],
+      playerBlue: this.symbols[Math.floor(Math.random()*3)]
+    })
+  }
+
+  render(){
+    return {
+      <div className="App">
+        <PlayerCard 
+          color="red"
+          symbol={this.state.playerRed}  />
+        <PlayerCard 
+          color="blue"
+          symbol={this.state.playerBlue} />
+        <button onclick={this.runGame}>Run Game</button>
+      </div>
+    };
+  }
 }
 
 export default App;
